5a687ec049bc51db3e70fefaa7d296db591e5371
Евгения Крюгер <Evgenia.Kryuger@yandex.ru>
2024-07-10 03:06:45 +0300

    Добавлена функция фильтрации по состоянию транзакции

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,11 @@
-def filter_by_state():
+def filter_by_state(list_of_transactions: list[dict], transaction_state: str = 'EXECUTED') -> list[dict]:
     """Функция возвращает новый список словарей, у которых ключ state соответствует указанному значению"""
-    pass
+    new_list_of_transactions = []
+    for transaction in list_of_transactions:
+        if transaction['state'] == transaction_state:
+            new_list_of_transactions.append(transaction)
+    print(new_list_of_transactions)
+    return new_list_of_transactions
 
 def sort_by_date():
     """Функция возвращает новый список, отсортированный по дате"""
