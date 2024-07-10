fb754c2af886aacb76b08380d34ac1642fc18e1c
Евгения Крюгер <Evgenia.Kryuger@yandex.ru>
2024-07-11 00:14:43 +0300

    Тесты get_mask_account

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -8,3 +8,7 @@ def test_get_mask_card_number(return_card_number):
 @pytest.mark.parametrize("input_card_number_length, return_card_number_length", [("123456789012345678", "1234 56** **** 5678"), ("", " ** **** ")])
 def test_get_mask_card_number_length(input_card_number_length, return_card_number_length):
     assert get_mask_card_number(input_card_number_length) == return_card_number_length
+
+@pytest.mark.parametrize("input_get_mask_account, return_get_mask_account", [("123456789012345678", "**5678"), ("", "**"), ("123456789", "**6789"), ("73654108430135874305", "**4305")])
+def test_get_mask_account(input_get_mask_account, return_get_mask_account):
+    assert get_mask_account(input_get_mask_account) == return_get_mask_account
