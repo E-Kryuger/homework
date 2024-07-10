5d139cf500313e724b2a46d21d145cdb285d0e3c
Евгения Крюгер <Evgenia.Kryuger@yandex.ru>
2024-07-10 03:25:00 +0300

    Добавлена функция сортировки по дате

--- src/processing.py
+++ src/processing.py
@@ -7,7 +7,9 @@ def filter_by_state(list_of_transactions: list[dict], transaction_state: str = '
     print(new_list_of_transactions)
     return new_list_of_transactions
 
-def sort_by_date():
+def sort_by_date(list_of_transactions: list[dict], sort_in_descending_order: bool = True) -> list[dict]:
     """Функция возвращает новый список, отсортированный по дате"""
-    pass
+    sorted_transactions = sorted(list_of_transactions, key=lambda transaction: transaction['date'], reverse=sort_in_descending_order)
+    print(sorted_transactions)
+    return sorted_transactions
 
